d24969c83c629fce07ebb231f423a04ccb18ed8f
GreyRobe <kingdon909@gmail.com>
2017-05-06 21:09:33 -0400

    Basic reader functionality
    It can now read from a text file and can be started and stopped with a button.

--- WordRunnerSmartwatch/app/src/main/java/com/apps/greyrobe/wordrunner/MainActivity.java
+++ WordRunnerSmartwatch/app/src/main/java/com/apps/greyrobe/wordrunner/MainActivity.java
@@ -3,11 +3,21 @@ package com.apps.greyrobe.wordrunner;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.wearable.view.WatchViewStub;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class MainActivity extends Activity {
 
     private TextView mTextView;
+    private Button toggleBtn;
+    private Thread storyReader;
+    private String[] story;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +28,73 @@ public class MainActivity extends Activity {
             @Override
             public void onLayoutInflated(WatchViewStub stub) {
                 mTextView = (TextView) stub.findViewById(R.id.text);
+                toggleBtn = (Button) stub.findViewById(R.id.btn_toggle);
+                loadStory();
+                createReaderThread();
             }
         });
+    }
+
+    public void toggleButton(View view)
+    {
+        if (toggleBtn.getText().equals("Start"))
+        {
+            storyReader.start();
+            toggleBtn.setText("Stop");
+        }
+        else
+        {
+            storyReader.interrupt();
+            toggleBtn.setText("Start");
+        }
+    }
+
+    private void createReaderThread()
+    {
+        storyReader = new Thread()
+        {
+            int wordNum = 0;
+            Thread wordSetterThread = new Thread()
+            {
+                @Override
+                public void run() {
+                    mTextView.setText(story[wordNum]);
+                }
+            };
+            @Override
+            public void run()
+            {
+                try
+                {
+                    for(; wordNum < story.length; wordNum++)
+                    {
+                        runOnUiThread(wordSetterThread);
+                        Thread.sleep(200 + story[wordNum].length()*10);
+                    }
+                }
+                catch (InterruptedException e) {}
+            }
+        };
+    }
+
+    private void loadStory()
+    {
+        try {
+            String str = "";
+            StringBuffer buf = new StringBuffer();
+            InputStream is = this.getResources().openRawResource(R.raw.story);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+            if (is != null) {
+                while ((str = reader.readLine()) != null) {
+                    buf.append(str + "\n");
+                }
+            }
+            story = buf.toString().split(" ");
+            is.close();
+        }
+        catch (IOException e)
+        {
+            mTextView.setText("Couldn't load file");
+        }
     }
 }
